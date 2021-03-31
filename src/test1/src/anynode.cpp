e4d28c21d10bbee9bb2bbcd382c0757457f1feb3
hui789456123@126.com <hui789456123@126.com>
2021-03-31 10:02:56 +0800

    v2

--- src/test1/src/anynode.cpp
+++ src/test1/src/anynode.cpp
@@ -2,6 +2,7 @@
 #include "std_msgs/String.h"
 #include <sstream>
 #include "geometry_msgs/PoseStamped.h"
+#include "math_utils.h"
 
 using namespace std;
 //string topic[num];
@@ -16,25 +17,70 @@ void topicCallback(const std_msgs::String::ConstPtr &msg)
     ROS_INFO("Node%d [ I heard ( %s ) ]", me, msg->data.c_str());
 }
 
+
+void mocap_cb(const geometry_msgs::PoseStamped::ConstPtr &msg)
+{
+    //---------------------------------------vicon定位相关------------------------------------------
+    Eigen::Vector3d pos_drone_mocap; //无人机当前位置 (vicon)
+    Eigen::Quaterniond q_mocap;
+    Eigen::Vector3d Euler_mocap; //无人机当前姿态 (vicon)
+    
+    Eigen::Vector3f pos_offset;
+    ros::Time last_timestamp;
+     
+    //位置 -- optitrack系 到 ENU系
+    //Frame convention 0: Z-up -- 1: Y-up (See the configuration in the motive software)
+    int optitrack_frame = 0;
+    if (optitrack_frame == 0)
+    {
+        // Read the Drone Position from the Vrpn Package [Frame: Vicon]  (Vicon to ENU frame)
+        pos_drone_mocap = Eigen::Vector3d(msg->pose.position.x, msg->pose.position.y, msg->pose.position.z);  //无人机当前位置 (vicon)
+
+        pos_drone_mocap[0] = pos_drone_mocap[0] - pos_offset[0];
+        pos_drone_mocap[1] = pos_drone_mocap[1] - pos_offset[1];
+        pos_drone_mocap[2] = pos_drone_mocap[2] - pos_offset[2];
+        // Read the Quaternion from the Vrpn Package [Frame: Vicon[ENU]]
+        q_mocap = Eigen::Quaterniond(msg->pose.orientation.w, msg->pose.orientation.x, msg->pose.orientation.y, msg->pose.orientation.z);
+    }
+    else
+    {
+        // Read the Drone Position from the Vrpn Package [Frame: Vicon]  (Vicon to ENU frame)
+        pos_drone_mocap = Eigen::Vector3d(-msg->pose.position.x, msg->pose.position.z, msg->pose.position.y);
+        // Read the Quaternion from the Vrpn Package [Frame: Vicon[ENU]]
+        q_mocap = Eigen::Quaterniond(msg->pose.orientation.w, msg->pose.orientation.x, msg->pose.orientation.z, msg->pose.orientation.y); //Y-up convention, switch the q2 & q3
+        pos_drone_mocap[0] = pos_drone_mocap[0] - pos_offset[0];
+        pos_drone_mocap[1] = pos_drone_mocap[1] - pos_offset[1];
+        pos_drone_mocap[2] = pos_drone_mocap[2] - pos_offset[2];
+    }
+
+    // Transform the Quaternion to Euler Angles  将四元数变换为欧拉角
+    Euler_mocap = quaternion_to_euler(q_mocap);
+    last_timestamp = msg->header.stamp;
+}
+
+
 int main(int argc, char **argv)
 {
     //用于解析ROS参数，第三个参数为本节点名
     ros::init(argc, argv,"anynode");
     //实例化句柄，初始化node
     ros::NodeHandle nh("~");
-    //定义发布的频率 
-    ros::Rate loop_rate(1.0);
 
     int me  ;
     int nodenum;
     int num ;
+    int frequ ;
     string object_name;
     nh.param<int>("me", me, 0 ) ;    //当前节点, 为0则是master
     nh.param<int>("nodenum", nodenum, 2) ;     //一共有多少个节点
     nh.param<int>("num", num, 10) ;     //每个节点发布topic数量
+    nh.param<int>("frequ", frequ, 1) ;     //发布频率
     nh.param<string>("object_name", object_name, "UAV");
 
     ROS_INFO("Hello? Look at me! What are you afraid of?");
+
+    //定义发布的频率 
+    ros::Rate loop_rate(1);
 
     std::string myname = "node" ;
     string s = to_string(me);
@@ -50,7 +96,7 @@ int main(int argc, char **argv)
   { 
       subnum = (nodenum-1)*num;  
       // 【订阅】optitrack估计位置
-      //ros::Subscriber optitrack_sub = nh.subscribe<geometry_msgs::PoseStamped>("/vrpn_client_node/"+ object_name + "/pose", 100, mocap_cb);
+      ros::Subscriber optitrack_sub = nh.subscribe<geometry_msgs::PoseStamped>("/vrpn_client_node/"+ object_name + "/pose", 100, mocap_cb);
           for(int i = 1; i <=num; i++) 
         {   
         //std_msgs::String为需要public的类型
